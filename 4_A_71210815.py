3cd7eb8ea5dcc78817b479671bdc199995c972ec
jonathan <jonathanimmanuel03@gmail.com>
2021-12-03 05:09:04 +0800

    Soal 4

---
+++ 4_A_71210815.py
@@ -0,0 +1,25 @@
+import os
+os.system("cls")
+
+string = ""
+
+x = int(input("Input : "))
+bar = x
+while bar >= 0:
+	kol = bar
+	while kol > 0:
+		string = string + "   "
+		kol = kol - 1	
+	kiri = 1
+	while kiri < (x - (bar-1)):
+		string = string + " * "
+		kiri = kiri + 1		
+	kanan = 1
+	while kanan < kiri -1:
+		string = string + " * "
+		kanan = kanan + 1	
+
+	string = string + "\n\n"
+	bar = bar - 1
+	
+print (string)
